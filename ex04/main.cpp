56094c6355030af51b1b132824e3a3876ab2dd2b
Aimaadd <abentaye@mi-r4-p5.s19.be>
2024-10-13 16:16:31 +0200

    sed is for winners

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,39 +2,42 @@
 #include <fstream>
 #include <string>
 
-int main(int argc, char **argv) {
+std::string toReplace(std::string buffer, const std::string& s1, const std::string& s2) {
+    size_t pos = 0;
+    while ((pos = buffer.find(s1, pos)) != std::string::npos) {
+        buffer.replace(pos, s1.length(), s2);
+        pos += s2.length();
+    }
+    return buffer;
+}
 
+int main(int argc, char **argv) {
     if (argc != 4) {
         std::cout << "Error: wrong amount of arguments" << std::endl;
         return (1);
     }
+
     std::ifstream inputFile(argv[1]);
-    std::string replace;
-    std::string line;
-    std::ofstream fio(argv[1], std::ios::app);
-    if (fio.is_open()) {
-        std::cout << "file successfully opened" << std::endl;
-        replace = argv[1];
-        replace.append(".replace");
-        std::cout << "file name : " << replace << std::endl;
-        std::cout << "file content : " << std::endl;
-        std::ofstream file(replace, std::ios::app);
-        while (getline(inputFile, line)) {
-            if (inputFile.eof()) {
-                file << line;
-                break;
-            }
-            if (line.empty())
-                file << std::endl;
-            else
-                file << line << std::endl;
-        }
-    }
-    else {
+    if (!inputFile.is_open()) {
         std::cout << "Error: can't open file" << std::endl;
         return (1);
     }
-    fio.close();
-    std::cout << "file successfully closed" << std::endl;
+
+    std::string replaceFileName = std::string(argv[1]) + ".replace";
+    std::ofstream replaceFile(replaceFileName);
+    if (!replaceFile.is_open()) {
+        std::cout << "Error: can't create replace file" << std::endl;
+        return (1);
+    }
+
+    std::string line;
+    while (getline(inputFile, line)) {
+        line = toReplace(line, argv[2], argv[3]);
+        replaceFile << line << std::endl;
+    }
+
+    inputFile.close();
+    replaceFile.close();
+    std::cout << "File successfully processed" << std::endl;
     return (0);
 }
